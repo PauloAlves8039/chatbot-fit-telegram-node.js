9903bf13e502b2973d85dadf24f75cb0001f58c4
PauloAlves8039 <pj38alves@gmail.com>
2020-08-22 16:02:35 -0300

    Implementando lógica para uso do Dialogflow

--- dialogflow.js
+++ dialogflow.js
@@ -4,3 +4,42 @@
  * @description: responsável pela integração com a API do Dialogflow - https://dialogflow.cloud.google.com/.
  * @version 1.0.0 (22/08/2020)
  */
+
+const dialogflow = require('dialogflow');
+const configs = require('./dio-bot-fit.json');
+
+const sessionsClient = new dialogflow.SessionsClient({
+    projectId: configs.project_id,
+    credentials: {
+        private_key: configs.private_key,
+        client_email: configs.client_email
+    }
+});
+
+/**
+ * Responsável por gerar mensagens do Dialogflow.
+ * 
+ * @function sendMessage
+ * 
+ * @param  chatId identifica o id do chatbot.
+ * @param  message representa o corpo da mensagem.
+ */
+async function sendMessage(chatId, message){
+    
+    const sessionPath = sessionsClient.sessionPath(configs.project_id, chatId);
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: message,
+                languageCode: 'pt-br'
+            }
+        }
+    }
+
+    const responses = await sessionsClient.detectIntent(request);
+    const result = responses[0].queryResult;
+    console.log(JSON.stringify(result, null, 2));
+};
+
+sendMessage('12938123', 'oi');
