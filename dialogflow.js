fbdf4432d4bbb3e3dcc8cef51445e58370c03d23
PauloAlves8039 <pj38alves@gmail.com>
2020-08-22 12:36:32 -0300

    Integração inicial com o Dialogflow

---
+++ dialogflow.js
@@ -0,0 +1,6 @@
+/**
+ * @file: dialogflow.js
+ * @author: Paulo Alves
+ * @description: responsável pela integração com a API do Dialogflow - https://dialogflow.cloud.google.com/.
+ * @version 1.0.0 (22/08/2020)
+ */
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 /**
- * @file: index.ts
+ * @file: index.js
  * @author: Paulo Alves
  * @description: responsável pela integração com a API do Telegram para uso de um chatbot.
  * @version 1.0.0 (22/08/2020)
  */
 
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = 'DIGITE AQUI SEU TOKEN!!!';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', function (msg) {
+    const chatId = msg.chat.id;
+    console.log(msg.text);
+    bot.sendMessage(chatId, 'Obrigado por sua mensagem!');
+});
